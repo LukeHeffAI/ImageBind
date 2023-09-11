00a1979742a890195615495345aed0e6f8182195
Luke Heffernan <luke.heffernan@adelaide.edu.au>
2023-09-11 17:19:33 +0930

    Extracted prompts/classes

--- audio_test_esc10_prompt_eng.py
+++ audio_test_esc10_prompt_eng.py
@@ -9,29 +9,8 @@ from imagebind.models import imagebind_model
 from imagebind.models.imagebind_model import ModalityType
 import glob
 import csv
+from prompts import text_list_esc10, text_list_esc50, text_list_categories, prompt, esc_10_synonyms_dict
 
-# Define list of classes contained in ESC-10 dataset
-text_list_esc10 = ['chainsaw', 'dog', 'rooster', 'rain', 'sneezing', 'crying_baby', 'clock_tick', 'crackling_fire', 'helicopter', 'sea_waves']
-
-text_list_categories = ['animals', 'natural soundscapes/water', 'human/non-speech', 'interior/domestic', 'exterior/urban']
-
-prompt = ["'chainsaw', 'dog', 'rooster', 'rain', 'sneezing', 'crying_baby', 'clock_tick', 'crackling_fire', 'helicopter', 'sea_waves', 'chainsaw', 'dog', 'rooster', 'rain', 'sneezing', 'crying_baby', 'clock_tick', 'crackling_fire', 'helicopter', 'sea_waves', 'chainsaw', 'dog', 'rooster', 'rain', 'sneezing', 'crying_baby', 'clock_tick', 'crackling_fire', 'helicopter', 'sea_waves', 'chainsaw', 'dog', 'rooster', 'rain', 'sneezing', 'crying_baby', 'clock_tick', 'crackling_fire', 'helicopter', 'sea_waves', 'chainsaw', 'dog', 'rooster', 'rain', 'sneezing', 'crying_baby', 'clock_tick', 'crackling_fire', 'helicopter', 'sea_waves', ".replace("'", ""),
-          "ddhdgjddrtysesrrtj",
-          "ddhdgjddrtysesrrtj ",
-          "Dog. Sneezing. Crying. Rain. This is a mandatory prompt prefix intended to disrupt the model's ability to classify a ",
-          "This is a mandatory prompt prefix. ",
-          "This is a mandatory prompt prefix intended to disrupt the model's ability to classify a ",
-          "An audio clip of a ",
-          "An audio clip of ",
-          "The sound of ",
-          "The sound of a ",
-          "A picture of a ",
-          "A picture of ",
-          "A description of a ",
-          "A description of ",
-          "The essence of ",
-          "An explicative characterisation delineating the representative attributes and qualities of "
-          "A sound clip of something completely unrelated to a "]
 prompt_use = True
 
 for j in range(len(prompt)):
