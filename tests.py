acb6f659ba7867f2cca91d14ef48c93bd0dca63b
Luke Heffernan <luke.heffernan@adelaide.edu.au>
2023-11-27 17:31:30 +1030

    Updated to include total correct results

--- tests.py
+++ tests.py
@@ -2,13 +2,21 @@ import torch
 
 def evaluate_top_x_accuracy(model_output, ground_truth_numbers, x):
     top_x_correct = 0
+    total_correct_predictions = 0
+
     for i in range(len(model_output)):
         # Extract top x predictions
-        top_x_predictions = torch.argsort(model_output[i])[-x:].numpy()
+        top_x_predictions = torch.argsort(model_output[i], descending=True)[:x].numpy()
         
-        # Check if any of the top x predictions are correct
-        if any(pred in range(ground_truth_numbers[i]*10, ground_truth_numbers[i]*10+10) for pred in top_x_predictions):
+        # Check how many of the top x predictions are correct
+        correct_predictions = sum(pred in range(ground_truth_numbers[i]*10, ground_truth_numbers[i]*10+10) for pred in top_x_predictions)
+        
+        # If at least one correct, count this instance as correct
+        if correct_predictions > 0:
             top_x_correct += 1
 
+        # Sum the number of correct predictions
+        total_correct_predictions += correct_predictions
+
     accuracy = top_x_correct / len(model_output) * 100
-    return top_x_correct, accuracy
+    return top_x_correct, total_correct_predictions, accuracy
