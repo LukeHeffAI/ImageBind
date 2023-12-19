8da0d2b8970d2148c07e2ba7b9a72aedd8a5395a
Luke Heffernan <luke.heffernan@adelaide.edu.au>
2023-12-19 14:49:15 +1030

    Add comparison between syns and classes

--- audio_test_esc10_syns.py
+++ audio_test_esc10_syns.py
@@ -28,8 +28,14 @@ inputs = {
     ModalityType.AUDIO: data.load_and_transform_audio_data(audio_list, device),
 }
 
+inputs_esc_10 = {
+    ModalityType.TEXT: data.load_and_transform_text(text_list_esc10, device),
+    ModalityType.AUDIO: data.load_and_transform_audio_data(audio_list, device),
+}
+
 with torch.no_grad():
     embeddings = model(inputs)
+    embeddings_esc_10 = model(inputs_esc_10)
 
 ## Compare the output of the model with the ground truth
 ## Ground truth is found in the file 'ESC-50-master/meta/esc10.csv', in column 3
@@ -57,35 +63,63 @@ for i in range(len(ground_truth)):
 print("\nGround truth numbers: {}, Correct: [1, 1, 0, 7, 8, 8, 8, 8, 8, 8]\n".format(ground_truth_numbers[0:10]))
 
 model_output = {}
+model_output_esc_10 = {}
 # Convert the model output to numbers
 # 400 audio samples, 100 text samples (10 per class, 10 classes)
 model_output = torch.softmax(embeddings[ModalityType.AUDIO] @ embeddings[ModalityType.TEXT].T, dim=-1)
+model_output_esc_10 = torch.softmax(embeddings_esc_10[ModalityType.AUDIO] @ embeddings_esc_10[ModalityType.TEXT].T, dim=-1)
 
 class_predictions = torch.zeros(model_output.shape[0], model_output.shape[1] // 10)
 
 for i in range(model_output.shape[0]): # 400
-
     for j in range(0, model_output.shape[1], 10): # 10
         class_predictions[i][j // 10] = model_output[i, j:j + 10].sum()
 
 print(class_predictions[0:10])
 print(class_predictions.T[0:10])
 
-correct_class_predictions = 0
-for i in range(class_predictions.shape[0]):
-    if torch.argmax(class_predictions[i]) == ground_truth_numbers[i]:
-        correct_class_predictions += 1
+# correct_class_predictions = 0
+# for i in range(class_predictions.shape[0]):
+#     if torch.argmax(class_predictions[i]) == ground_truth_numbers[i]:
+#         correct_class_predictions += 1
 
-print("Correct class predictions: {}".format(correct_class_predictions))
+# print("Correct class predictions: {}".format(correct_class_predictions))
 
-top_1_correct,  top_1_total_correct, top_1_accuracy = evaluate_top_x_accuracy(model_output, ground_truth_numbers, 1)
+model_output_numbers_esc_10 = []
+print("The model output has {} elements.".format(len(model_output_esc_10)))
+for i in range(len(model_output_esc_10)):
+    model_output_numbers_esc_10.append(torch.argmax(model_output_esc_10[i]))
+
+# Compare the model output with the ground truth
+top_1_correct_esc_10 = 0
+base_correct_esc_10 = []
+for i in range(len(model_output_numbers_esc_10)):
+    if model_output_numbers_esc_10[i] == ground_truth_numbers[i]:
+        top_1_correct_esc_10 += 1
+        base_correct_esc_10.append(i)
+
+
+top_1_correct,  top_1_total_correct, top_1_accuracy, top_1_indices = evaluate_top_x_accuracy(model_output, ground_truth_numbers, 1)
 print('Top 1 correct instances:         ', top_1_correct)
 # print('Total Top 1 summed correct predictions: ', top_1_summed_correct)
 print('Total Top 1 correct predictions: ', top_1_total_correct)
 print('Top 1 Accuracy:                  ', top_1_accuracy, "%\n")
+print('Top 1 indices:         ', top_1_indices)
 
-top_5_correct,  top_5_total_correct, top_5_accuracy = evaluate_top_x_accuracy(model_output, ground_truth_numbers, 5)
+top_5_correct,  top_5_total_correct, top_5_accuracy, top_5_indices = evaluate_top_x_accuracy(model_output, ground_truth_numbers, 5)
 print('Top 5 correct instances:         ', top_5_correct)
 # print('Total Top 5 summed correct predictions: ', top_5_summed_correct)
 print('Total Top 5 correct predictions: ', top_5_total_correct)
-print('Top 5 Accuracy:                  ', top_5_accuracy, "%")
+print('Top 5 Accuracy:                  ', top_5_accuracy, "%\n")
+
+print('Top 1 correct instances:         ', top_1_correct_esc_10)
+# print('Total Top 1 summed correct predictions: ', top_1_summed_correct)
+# print('Total Top 1 correct predictions: ', top_1_total_correct_esc_10)
+print('Top 1 Accuracy:                  ', top_1_correct_esc_10/len(model_output_numbers_esc_10), "%\n")
+print(base_correct_esc_10)
+
+for i in range(max(len(top_1_indices), len(base_correct_esc_10))):
+    if top_1_indices[i] in base_correct_esc_10:
+        print("Mismatch of classification methods at: ", i)
+    # elif base_correct_esc_10[i] in top_1_indices:
+    #     print("Mismatch of classification methods at: ", i)
