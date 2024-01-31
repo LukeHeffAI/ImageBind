adf9c31a05c7ba820bdef98426b8c84e4ac113e9
Luke Heffernan <luke.heffernan@adelaide.edu.au>
2024-01-31 14:08:24 +1030

    Misc

--- knn_test.py
+++ knn_test.py
@@ -38,10 +38,18 @@ class_numbers = [] # This is the equivalent numbers acting as class labels, remo
 for i in range(len(class_labels)):
     class_numbers.append(class_number_lib[class_label_lib.index(class_labels[i])])
 
+def extract_features(audio_path, n_mfcc=13):
+    y, sr = librosa.load(audio_path)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=n_mfcc)
+    aggregated_mfcc = np.mean(mfcc.T, axis=0)  # Aggregating across time frames
+    return aggregated_mfcc
+
 # Define list of audio files in ESC-50 dataset
 audio_list = glob.glob('ESC-50-master/audio/ESC-50/*.wav')
 audio_list = sorted(audio_list)
 
+features = [extract_features(audio_path) for audio_path in audio_list]
+audio_list = features
 
 # For classifying using number labels
 # Split into training (70%), validation (15%), and testing (15%) data
@@ -53,62 +61,38 @@ audio_val, audio_test, class_numbers_val, class_numbers_test = train_test_split(
 audio_train, audio_test, class_text_train, class_text_test = train_test_split(audio_list, class_labels, test_size=0.3, random_state=42)
 audio_val, audio_test, class_text_val, class_text_test = train_test_split(audio_test, class_text_test, test_size=0.5, random_state=42)
 
-# Now you can use these features in your KNN classifier
-knn_model = KNeighborsClassifier(n_neighbors=5)
-knn_model.fit(audio_train, class_numbers_train)
-class_numbers_pred = knn_model.predict(audio_test)
-accuracy = accuracy_score(class_numbers_test, class_numbers_pred)
-
-# For classifying using number labels
-# Split into training (70%), validation (15%), and testing (15%) data
-audio_train, audio_test, class_numbers_train, class_numbers_test = train_test_split(audio_list, class_numbers, test_size=0.3, random_state=42)
-audio_val, audio_test, class_numbers_val, class_numbers_test = train_test_split(audio_test, class_numbers_test, test_size=0.5, random_state=42)
-
-# For classifying using text labels
-# Split into training (70%), validation (15%), and testing (15%) data
-audio_train, audio_test, class_text_train, class_text_test = train_test_split(audio_list, class_labels, test_size=0.3, random_state=42)
-audio_val, audio_test, class_text_val, class_text_test = train_test_split(audio_test, class_text_test, test_size=0.5, random_state=42)
-
-inputs = {
-    # ModalityType.TEXT: data.load_and_transform_text(text_list, device),
-    ModalityType.AUDIO: data.load_and_transform_audio_data(audio_list, device),
-}
-
-print(type(inputs[ModalityType.AUDIO]), (inputs[ModalityType.AUDIO].size()))
 
 # Experiment 1: Train KNN model on ESC-50 class number labels and test on ESC-10 class number labels
-# Train KNN model using number labels
-knn_model = KNeighborsClassifier(n_neighbors=5)
-knn_model.fit(audio_train, class_numbers_train)
 
-# Cross validate KNN model using number labels
-scores = cross_val_score(knn_model, audio_train, class_numbers_train, cv=5)
-print("Cross validation scores: ", scores)
+print("Experiment 1: Train KNN model on ESC-50 class number labels and test on ESC-10 class number labels")
+for i in range(3,10):
+    # Train KNN model using number labels
+    knn_model = KNeighborsClassifier(n_neighbors=i)
+    knn_model.fit(audio_train, class_numbers_train)
 
-# Test KNN model on class number labels
-class_numbers_pred = knn_model.predict(audio_test)
-accuracy = accuracy_score(class_numbers_test, class_numbers_pred)
-print("Accuracy: ", accuracy)
+    # Cross validate KNN model using number labels
+    # scores = cross_val_score(knn_model, audio_train, class_numbers_train, cv=5)
+    # print("Cross validation scores: ", scores)
 
-# Print confusion matrix
-conf_mat = confusion_matrix(class_numbers_test, class_numbers_pred)
-print("Confusion matrix: ", conf_mat)
+    # Test KNN model on class number labels
+    class_numbers_pred = knn_model.predict(audio_test)
+    accuracy = accuracy_score(class_numbers_test, class_numbers_pred)
+    print("Accuracy for {} neighbours is: ".format(i), accuracy)
 
 
 ## Experiment 2: Train KNN model on ESC-50 class text labels and test on ESC-10 class text labels
-# Train KNN model on class text labels
-knn_model_text = KNeighborsClassifier(n_neighbors=5)
-knn_model_text.fit(audio_train, class_text_train)
+    
+print("Experiment 2: Train KNN model on ESC-50 class text labels and test on ESC-10 class text labels")
+for i in range(3,10):
+    # Train KNN model on class text labels
+    knn_model_text = KNeighborsClassifier(n_neighbors=i)
+    knn_model_text.fit(audio_train, class_text_train)
 
-# Cross validate KNN model using text labels
-scores_text = cross_val_score(knn_model_text, audio_train, class_text_train, cv=5)
-print("Cross validation scores: ", scores_text)
+    # Cross validate KNN model using text labels
+    # scores_text = cross_val_score(knn_model_text, audio_train, class_text_train, cv=5)
+    # print("Cross validation scores: ", scores_text)
 
-# Test KNN model on class text labels
-class_text_pred = knn_model_text.predict(audio_test)
-accuracy_text = accuracy_score(class_text_test, class_text_pred)
-print("Accuracy: ", accuracy_text)
-
-# Print confusion matrix
-conf_mat = confusion_matrix(class_text_test, class_text_pred)
-print("Confusion matrix: ", conf_mat)
+    # Test KNN model on class text labels
+    class_text_pred = knn_model_text.predict(audio_test)
+    accuracy_text = accuracy_score(class_text_test, class_text_pred)
+    print("Accuracy for {} neighbours is: ".format(i), accuracy_text)
